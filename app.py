8436631dfe6323a669e9b3c82c29c6dfc776b4f5
SeurNaem1 <vualongho0@gmail.com>
2026-01-11 17:00:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def hello():
     return "Hello from Google Cloud Run!"
 
 if __name__ == "__main__":
-    # Cloud Run sẽ tự set PORT
     import os
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
